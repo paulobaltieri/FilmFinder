d1e76bfb344eb170655fd59b3d5447ffe750c115
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2024-10-25 13:54:07 -0300

    criado pagina home e adicionado novos elementos
    Foi adicionado um carousel na pagina inicial

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
