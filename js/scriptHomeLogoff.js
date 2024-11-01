5e4bf3eb5d611b12356d6f5d08feadf69d5c0537
Paulo Baltieri <baltieri_paulo@hotmail.com>
2024-10-31 21:59:36 -0300

    adicionado a configuração do botão sair da pagina home

--- js/scriptHomeLogoff.js
+++ js/scriptHomeLogoff.js
@@ -1,5 +1,18 @@
-const btnOff = document.querySelector(".is-warning")
+const btn = document.querySelector('.is-warning')
+const btnOff = document.querySelector('.is-danger')
+const modal = document.querySelector('.modal')
+const btnSuccess = document.querySelector('.is-success')
 
-btnOff.addEventListener('click', event => {
-    console.log('Apertou')
+
+btn.addEventListener('click', () => {
+    modal.classList.add("is-active")
+})
+
+btnOff.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+    modal.classList.remove('is-active')
+    location.href = "../index.html"
 })
