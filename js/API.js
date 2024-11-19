a844cb46147eab01667935c7d1686f2e0c79bd6d
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2024-11-19 16:25:06 -0300

    Refatorando codigo da API

--- js/API.js
+++ js/API.js
@@ -2,36 +2,29 @@ const movies = {
     movie01: {
         title: document.querySelector('.titleMovie1'),
         img: document.querySelector('.moviePoster1')
-    },
-    movie02: {
-        title: document.querySelector('.titleMovie2'),
-        img: document.querySelector('.moviePoster2')
-    },
-    movie03: {
-        title: document.querySelector('.titleMovie3'),
-        img: document.querySelector('.moviePoster3')
-    },
-    movie04: {
-        title: document.querySelector('.titleMovie4'),
-        img: document.querySelector('.moviePoster4')
     }
 }
 
-const movieAPI = (titleElement, imgElement, name) => {
-    fetch(`https://www.omdbapi.com/?t=${name}&apikey=b96734e4`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.Response === "True") {
-                titleElement.innerHTML = data.Title
-                imgElement.src = data.Poster
-            } else {
-                titleElement.innerHTML = "Não encontrado"
-                imgElement.src = "/img/notfound.png"
-            }
-        })
+const getData = async (title, img, name) => {
+    const response = await fetch(`https://www.omdbapi.com/?t=${name}&apikey=b96734e4`)
+
+    try {
+        const data = await response.json()
+
+        if (response.status !== 200) {
+            throw new Error("Dados não encontrados")
+        }
+
+        if (data.Response === "False") {
+            throw new Error("Erro ao obter dados ")
+        }
+
+        title.innerHTML = data.Title
+        img.src = data.Poster
+
+    } catch (erro) {
+        console.log("Mensagem", erro.message)
+    }
 }
 
-movieAPI(movies.movie01.title, movies.movie01.img, "Deadpool")
-movieAPI(movies.movie02.title, movies.movie02.img, "King Kong")
-movieAPI(movies.movie03.title, movies.movie03.img, "Logan")
-movieAPI(movies.movie04.title, movies.movie04.img, "Godzilla")
+getData(movies.movie01.title, movies.movie01.img, "Deadpool")
